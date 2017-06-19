5764978c56871b34fa12b828750c24f7eb31505e
fegonzalezg <fegonzalezg@outlook.com>
2017-06-18 22:13:40 -0400

    update2

--- app/src/main/java/com/example/felipe/smokeoff/PageTwoActivity.kt
+++ app/src/main/java/com/example/felipe/smokeoff/PageTwoActivity.kt
@@ -7,52 +7,74 @@ import android.content.Intent
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.TextView
-
-
-import java.util.Calendar
-
+import java.util.*
 
 
 var dataQuery: TextView? = null
 
 class PageTwoActivity : ActionBarActivity() {
 
+    var dateInputChooser: Int = 0
+    var mHandler = Handler()
+    var mInterval: Long = 1000
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_two)
 
-        dataQuery = findViewById(R.id.textView) as TextView
+        //dataQuery = findViewById(R.id.textView) as TextView
+        var isnew = 0
 
+        if (isnew==1) {
 
-        dataQuery!!.setOnClickListener {
             val userInputActivity = Intent(this, MainActivity::class.java)
             startActivityForResult(userInputActivity, 1)
-
-                    }
-
-
+        }
+        else{}
 
 
 
+        /*mHandler.postDelayed(object:Runnable {
+            override fun run(){
 
+                if (dateInputChooser == 1)
+                    TickTimer(DatePasser)
 
+                mHandler.postDelayed(this, mInterval)
+            }
+        }, mInterval )*/
 
-        /*//Get the Intent that started this activity and extract the string
-        val MessageReceiver = intent
-        val firstMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE1)
-        val secondMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE2)
-        val thirdMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE3)
-
-        // Capture the layout's TextView and set the string as its text
-        val firstTextView = findViewById(R.id.textView) as TextView
-        val secondTextView = findViewById(R.id.textView2) as TextView
-        val thirdTextView = findViewById(R.id.textView3) as TextView
-
-        firstTextView.text = firstMessage
-        secondTextView.text = secondMessage
-        thirdTextView.text = thirdMessage*/
     }
+
+/*    fun TickTimer(StartTime: Date) {
+
+        val NowDate = Date()
+        var Difference = NowDate.time - StartTime.time
+        val secondsInMilli: Long = 1000
+        val minutesInMilli: Long = secondsInMilli*60
+        val hoursInMilli: Long = minutesInMilli*60
+        val daysInMilli: Long = hoursInMilli*24
+
+        val elapsedDays: Long = Difference/daysInMilli
+        Difference = Difference % daysInMilli
+
+        val elapsedHours: Long = Difference/hoursInMilli
+        Difference = Difference % hoursInMilli
+
+        val elapsedMinutes: Long = Difference/minutesInMilli
+        Difference = Difference % minutesInMilli
+
+        val elapsedSeconds: Long = Difference/secondsInMilli
+
+        Timer!!.text = getString(R.string.timer, elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds)
+
+
+
+    }*/
 
     override fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent) {
         if (requestCode == 1)
@@ -71,3 +93,30 @@ class PageTwoActivity : ActionBarActivity() {
     }//onActivityResult
 
 }
+
+
+
+
+    /* dataQuery!!.setOnClickListener {
+          val userInputActivity = Intent(this, MainActivity::class.java)
+          startActivityForResult(userInputActivity, 1)
+
+                  }*/
+
+
+        /*//Get the Intent that started this activity and extract the string
+        val MessageReceiver = intent
+        val firstMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE1)
+        val secondMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE2)
+        val thirdMessage = MessageReceiver.getStringExtra(MainActivity.EXTRA_MESSAGE3)
+
+        // Capture the layout's TextView and set the string as its text
+        val firstTextView = findViewById(R.id.textView) as TextView
+        val secondTextView = findViewById(R.id.textView2) as TextView
+        val thirdTextView = findViewById(R.id.textView3) as TextView
+
+        firstTextView.text = firstMessage
+        secondTextView.text = secondMessage
+        thirdTextView.text = thirdMessage*/
+
+
